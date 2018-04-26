f9654691bd64f4c56aed8aac367c3c009390b5e8
Alex Wang <alexwang@Alexs-MacBook-Pro.local>
2018-04-26 16:17:53 +0800

    Update the delegatecall sample

--- OpcodeCallTest/OpcodeCallTest.sol
+++ OpcodeCallTest/OpcodeCallTest.sol
@@ -8,6 +8,8 @@ pragma solidity ^0.4.21;
 interface SubjectInterface {
     function getState() external returns(uint);
     function setState(uint newState) external; 
+    function getState2() external returns(uint);
+    function setState2(uint newState) external; 
 }
 
 contract CallingContract {
@@ -31,18 +33,52 @@ contract CallingContract {
         return _subject.getState();
     }
     
+    function setState2(uint newState) public payable{
+        emit PrintSender0(msg.sender);
+        emit PrintValue0(msg.value);
+        _subject.setState2(newState);
+    }
+    
+    function getState2() public payable returns(uint)  {
+        emit PrintSender0(msg.sender);
+        emit PrintValue0(msg.value);
+        return _subject.getState2();
+    }
+    
+}
+
+contract CloneContract {
+    function () public payable {
+        assembly {
+            let calldatastart := msize()
+            mstore(0x40, add(calldatastart, calldatasize))
+            calldatacopy(calldatastart, 0, calldatasize)
+            //resolver address
+            let res := delegatecall(div(mul(gas, 63), 64), 0x368ad6328db21f181cace7787a3eb323bc76576d, calldatastart, calldatasize, 0, 0)
+            let returndatastart := msize()
+            mstore(0x40, add(returndatastart, returndatasize))
+            returndatacopy(returndatastart, 0, returndatasize)
+            switch res case 0 { revert(returndatastart, returndatasize) } default { return(returndatastart, returndatasize) }
+        }
+    }
 }
 
 contract ForwardingContract {
+    // address private _subjectAddress;
     event PrintSender1(address senderAddress);
     event PrintValue1(uint amount);
     
+    //   constructor(address subjectAddress) public {
+    //     _subjectAddress = subjectAddress;
+    //    }
     
     function () public payable {
         emit PrintSender1(msg.sender);
         emit PrintValue1(msg.value);
-
+    
+        address subjectAddress = Router(0xf2bd5de8b57ebfc45dcee97524a7a08fccc80aef).getSubjectAddress();
         assembly {
+            // let subjectAddress := sload(0)
             let callDataAddress := msize()
             //mstore(p, v)	-	F	mem[p..(p+32)) := v
             //calldatasize	 	F	size of call data in bytes
@@ -52,9 +88,7 @@ contract ForwardingContract {
             //gas	 	F	gas still available to execution
             //let res := call(div(mul(gas, 63), 64), subjectAddress, 0, callDataAddress, calldatasize, 0, 0)
             //let res := callcode(div(mul(gas, 63), 64), subjectAddress, 0, callDataAddress, calldatasize, 0, 0)
-            //The address must been replaced by the SubjectContract address.This can not be done by store the address in a state variable 
-            //because after cdelegatecall the storage slot where  SubjectContract address is stored will be replaced by the same slot of SubjectContract
-            let res := delegatecall(div(mul(gas, 63), 64), 0xe3224c3ba5254ab1b36bac08fee6ab427f284f96, callDataAddress, calldatasize, 0, 0)
+            let res := delegatecall(div(mul(gas, 63), 64), subjectAddress, callDataAddress, calldatasize, 0, 0)
             let returnDataAddress := msize()
             //returndatasize	 	B	size of the last returndata
             mstore(0x40, add(returnDataAddress, returndatasize))
@@ -72,10 +106,23 @@ contract ForwardingContract {
     }
 }
 
+contract Router {
+    address subjectAddress;
+    
+    function getSubjectAddress() public view returns(address) {
+        return subjectAddress;
+    }
+    
+    function setSuebjectAddress(address newAddress) public{
+        subjectAddress = newAddress;
+    }
+}
+
 contract SubjectContract {
-     uint private state;
      uint private state2;
-     
+     uint private state;
+
+
      event PrintSender2(address senderAddress);
      event PrintValue2(uint amount);
      
@@ -91,8 +138,12 @@ contract SubjectContract {
          return state;
      }
      
-     function () public payable {
-         emit PrintSender2(msg.sender);
-         emit PrintValue2(msg.value);
+     function getState2() public view returns(uint) {
+         return state2;
      }
+     
+     function setState2(uint newState) public returns(uint) {
+         state2 = newState;
+     }
+
 }
